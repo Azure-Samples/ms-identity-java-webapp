385f11bc59b19e0ef78c4fa7f6731db9e050a878
sgonzalezMSFT <sagonzal@microsoft.com>
2019-08-01 12:04:10 -0700

    Update AppCreationScripts

--- AppCreationScripts/Cleanup.ps1
+++ AppCreationScripts/Cleanup.ps1
@@ -1,4 +1,13 @@
-param([Parameter(Mandatory=$false)][PSCredential]$Credential=$null, [Parameter(Mandatory=$false)][string]$TenantId)
+[CmdletBinding()]
+param(    
+    [PSCredential] $Credential,
+    [Parameter(Mandatory=$False, HelpMessage='Tenant ID (This is a GUID which represents the "Directory ID" of the AzureAD tenant into which you want to create the apps')]
+    [string] $tenantId
+)
+
+if ((Get-Module -ListAvailable -Name "AzureAD") -eq $null) { 
+    Install-Module "AzureAD" -Scope CurrentUser 
+} 
 Import-Module AzureAD
 $ErrorActionPreference = 'Stop'
 
@@ -8,15 +17,7 @@ Function Cleanup
 .Description
 This function removes the Azure AD applications for the sample. These applications were created by the Configure.ps1 script
 #>
-   [CmdletBinding()]
-    param(
-        [Parameter(HelpMessage='Tenant ID (This is a GUID which represents the "Directory ID" of the AzureAD tenant into which you want to create the apps')]
-        [PSCredential] $Credential,
-        [string] $tenantId
-    )
 
-   process
-   {
     # $tenantId is the Active Directory Tenant. This is a GUID which represents the "Directory ID" of the AzureAD tenant 
     # into which you want to create the apps. Look it up in the Azure portal in the "Properties" of the Azure AD. 
 
@@ -48,15 +49,15 @@ This function removes the Azure AD applications for the sample. These applicatio
     # Removes the applications
     Write-Host "Cleaning-up applications from tenant '$tenantName'"
 
-    Write-Host "Removing 'app' (Webapp-Openidconnect) if needed"
-    $app=Get-AzureADApplication -Filter "identifierUris/any(uri:uri eq 'https://$tenantName/Webapp-Openidconnect')"  
+    Write-Host "Removing 'java-web-app' if needed"
+    $app=Get-AzureADApplication -Filter "DisplayName eq 'MyWebApp'"  
+
     if ($app)
     {
         Remove-AzureADApplication -ObjectId $app.ObjectId
         Write-Host "Removed."
     }
 
-   }
 }
 
 Cleanup -Credential $Credential -tenantId $TenantId
--- README.md
+++ README.md
@@ -107,8 +107,8 @@ Click on **save**.
 ### Step 4:  Configure the sample to use your Azure AD tenant
 
 Open `application.properties` in the src/main/resources folder. Fill in with your tenant and app registration information 
-noted in registration step. Replace 'YOUR_TENANT_NAME' with the tenant id, 'YOUR_CLIENT_ID' with the Application Id and 
-'YOUR_CLIENT_SECRET' with the key value noted.
+noted in registration step. Replace YOUR_TENANT_NAME with the tenant id, YOUR_CLIENT_ID with the Application Id and 
+YOUR_CLIENT_SECRET with the key value noted.
 
 If you did not use the  default redirect URIs, then you'll have to update `aad.redirectUriSignin` and 
 `aad.redirectUriGraphUsers` as well with the registered redirect URIs. 
